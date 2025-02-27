43f373f89ba3c5b81a7d3e978a677375d0b27fa9
louissonofhud <louis.hud10@gmail.com>
2025-02-27 13:44:45 +0000

    adding sidebar

--- main_ui/urls.py
+++ main_ui/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.create_issue, name="create_issue"),
+    path("", views.home, name="home"),
+    path("create/", views.create_issue, name="create_issue"),
     path("all_issues/", views.list_issues, name="list_issues"),
 ]
